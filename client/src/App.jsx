ecbf02087019706613c5071dc2b62d8902558c90
tharun5343 <tharun5343@gmail.com>
2024-06-25 18:24:10 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='bg-sky-300 text-center text-2xl'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
